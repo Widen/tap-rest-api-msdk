8901558b64b74bf87716f0a7d937849100aca025
Josh Lloyd <jlloyd@ip-10-0-0-10.ec2.internal>
2022-02-15 16:44:30 -0700

    added tests for old functionality as well as proposed pagination

--- tap_rest_api_msdk/tests/test_core.py
+++ tap_rest_api_msdk/tests/test_core.py
@@ -1,48 +1,19 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import json
+import pytest
+import requests
 
-from tap_rest_api_msdk.utils import flatten_json
+from tap_rest_api_msdk.tap import TapRestApiMsdk
+from tap_rest_api_msdk.tests.test_streams import url_path, json_resp, config
 
-SAMPLE_CONFIG = {
-    "api_url": "https://url.test",
-    "auth_method": "no_auth",
-    "auth_token": "",
-    "streams_config": [{
-        'name': 'test_name',
-        'path': 'path_test',
-        'primary_keys': ['key1', 'key2'],
-        'replication_key': 'key3'
-    }],
-    "records_path": "$.jsonpath[*]",
-}
+from singer_sdk.testing import get_standard_tap_tests
 
 
 # Run standard built-in tap tests from the SDK:
-# def test_standard_tap_tests():
-#     """Run standard tap tests from the SDK."""
-#     tests = get_standard_tap_tests(TapRestApi, config=SAMPLE_CONFIG)
-#     for test in tests:
-#         test()
+def test_standard_tap_tests(requests_mock):
+    """Run standard tap tests from the SDK."""
+    requests_mock.get(url_path(), json=json_resp())
+    tests = get_standard_tap_tests(TapRestApiMsdk, config=config())
+    for test in tests:
+        test()
 
-
-def test_flatten_json():
-    d = {
-        'a': 1,
-        'b': {
-            'a': 2,
-            'b': {'a': 3},
-            'c': {'a': "bacon", 'b': "yum"}
-        },
-        'c': [{'foo': 'bar'}, {'eggs': 'spam'}],
-        'd': [4, 5],
-        'e.-f': 6,
-    }
-    ret = flatten_json(d, except_keys=['b_c'])
-    assert ret['a'] == 1
-    assert ret['b_a'] == 2
-    assert ret['b_b_a'] == 3
-    assert ret['b_c'] == json.dumps({'a': "bacon", 'b': "yum"})
-    assert ret['c'] == json.dumps([{'foo': 'bar'}, {'eggs': 'spam'}])
-    assert ret['d'] == json.dumps([4, 5])
-    assert ret['e__f'] == 6
