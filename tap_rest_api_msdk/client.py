8b72d0075e7d8f0a5111e7561845267898147982
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2023-07-10 17:57:54 +1200

    Add support for expirying OAuth Tokens

--- tap_rest_api_msdk/client.py
+++ tap_rest_api_msdk/client.py
@@ -16,6 +16,8 @@ class RestApiStream(RESTStream):
 
     # Intialise self.http_auth used by prepare_request
     http_auth = None
+    # Cache the authenticator using a Smart Singleton pattern 
+    _authenticator = None
 
     @property
     def url_base(self) -> Any:
@@ -29,7 +31,6 @@ class RestApiStream(RESTStream):
       
 
     @property
-    @cached
     def authenticator(self) -> Any:
         """Calls an appropriate SDK Authentication method based on the the set auth_method
         which is set via the config.
@@ -39,9 +40,8 @@ class RestApiStream(RESTStream):
         Note 1: Each auth method requires certain configuration to be present see README.md
         for each auth methods configuration requirements.
         
-        Note 2: The cached decorator will have all calls to the authenticator use the
-        same instance logic to speed up processing. TODO: Examine if this affects OAuth2
-        callbacks and if logic is required for callback for expiring AWS STS tokens.
+        Note 2: Using Singleton Pattern on the autenticator for caching with a check
+        if an OAuth Token has expired and needs to be refreshed.
 
         Raises:
             ValueError: if the auth_method is unknown.
@@ -50,11 +50,16 @@ class RestApiStream(RESTStream):
             A SDK Authenticator or APIAuthenticatorBase if no auth_method supplied.
         """
 
-        stream_authenticator = select_authenticator(self)
-        
-        if stream_authenticator:
-            return stream_authenticator
-        else:
-            return APIAuthenticatorBase(stream=self)
-          
+        auth_method = self.config.get("auth_method",None)
 
+        if not self._authenticator:
+            self._authenticator = select_authenticator(self)         
+            if not self._authenticator:
+                # No Auth Method, use default Authenticator
+                self._authenticator = APIAuthenticatorBase(stream=self)           
+        elif auth_method == 'oauth':
+            if not self._authenticator.is_token_valid():
+                # Obtain a new OAuth token as it has expired
+                self._authenticator = select_authenticator(self)
+
+        return self._authenticator
