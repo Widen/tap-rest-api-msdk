94316baccea9ff7f596c82f91e5d66f6599cc93e
Josh lloyd <josh.lloyd@acquia.com>
2024-09-03 13:06:41 -0600

    fixed linting issues

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-from singer_sdk.testing import get_standard_tap_tests
+from singer_sdk.testing import get_tap_test_class
 from tap_rest_api_msdk.tap import TapRestApiMsdk
 
 from tests.test_streams import config, json_resp, url_path
@@ -10,6 +10,4 @@ from tests.test_streams import config, json_resp, url_path
 def test_standard_tap_tests(requests_mock):
     """Run standard tap tests from the SDK."""
     requests_mock.get(url_path(), json=json_resp())
-    tests = get_standard_tap_tests(TapRestApiMsdk, config=config())
-    for test in tests:
-        test()
+    get_tap_test_class(TapRestApiMsdk, config=config())
